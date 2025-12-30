c469dafbd7231fdeed5222a1bd19cda0b881e456
abdulrahman hassan <h4554n.abdul@gmail.com>
2025-12-30 05:27:45 +0100

    tailwind fixed

---
+++ src/app.rs
@@ -0,0 +1,63 @@
+use leptos::prelude::*;
+use leptos_meta::{provide_meta_context, Stylesheet, Title};
+use leptos_router::{
+    components::{Route, Router, Routes},
+    StaticSegment, WildcardSegment,
+};
+
+#[component]
+pub fn App() -> impl IntoView {
+    // Provides context that manages stylesheets, titles, meta tags, etc.
+    provide_meta_context();
+
+    view! {
+        // Leptos-generated CSS
+        <Stylesheet id="leptos" href="/pkg/dashboard-app-rs.css"/>
+
+        // Tailwind CSS
+        <link rel="stylesheet" href="/style/output.css"/>
+
+        // Document title
+        <Title text="Welcome to Leptos"/>
+
+        // App content
+        <Router>
+            <main>
+                <Routes fallback=move || "Not found.">
+                    <Route path=StaticSegment("") view=HomePage/>
+                    <Route path=WildcardSegment("any") view=NotFound/>
+                </Routes>
+            </main>
+        </Router>
+    }
+}
+
+/// Renders the home page of your application.
+#[component]
+fn HomePage() -> impl IntoView {
+    let count = RwSignal::new(0);
+    let on_click = move |_| *count.write() += 1;
+
+    view! {
+        <h1>"Welcome to my first Leptos!"</h1>
+        <button 
+            class="bg-red-500 rounded text-white px-2 py-2" 
+            on:click=on_click>
+            "Click Me: " {count}
+        </button>
+    }
+}
+
+/// 404 - Not Found
+#[component]
+fn NotFound() -> impl IntoView {
+    #[cfg(feature = "ssr")]
+    {
+        let resp = expect_context::<leptos_actix::ResponseOptions>();
+        resp.set_status(actix_web::http::StatusCode::NOT_FOUND);
+    }
+
+    view! {
+        <h1>"Not Found"</h1>
+    }
+}
